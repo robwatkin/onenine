0d9d0a5937f2ef6b0d1ebd4eddf9d74cd3751822
Rob Watkin <robwatkin@gmail.com>
2013-12-16 22:34:36 +0000

    Hello One Nine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onenine::Application.config.secret_token = '5c403f454c4559605a6839e448d30157189ba15b053bc825817a133eb7b39a210b5cb7960f925303c4b424ad86c2b4280bb3827cf432475ecd6bb960cc345712'
